20932178ffbd60d051a6327dddd1bda79df50ba3
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-06 17:47:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-two', function (req, res) {
+    res.sendfile('ärticle two is here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
