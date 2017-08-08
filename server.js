210079b3e82b346ac213948f75b577a57643a7c9
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-08 13:21:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlename', function (req, res) {
-    var articlename =re.params.articlename;
+    var articlename =req.params.articlename;
     res.send(createtemplate(articles[articlename]));
 });
 
