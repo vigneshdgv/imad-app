861b816c26789a5a2d326c534833cb873949daa9
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-09 16:11:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById("maint");
 element.innerHTML="This Website is Blocked";
-
 var img=document.getElementById("imad");
 img.onclick = function () {
     img.style.marginleft= "100px";
