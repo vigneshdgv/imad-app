21dfacc6b0de68bdc1d4d07ae2cdb93178b84bec
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-10 17:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ img.onclick = function () {
 var button = document.getElementById("counter");
 button.onclick = function(){
     var request =new XMLHttpRequest();
-    request.onreadyStatechange = function(){
+    request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status===200){
                 var counter = request.responseText;
