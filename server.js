3d29a5106baf5e9dbd24623d1e9e1c9abe71dd19
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-10 10:17:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 var counter=0;
 app.get('/counter', function(req, res) {
    counter = counter + 1;
