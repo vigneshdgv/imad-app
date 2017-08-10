13f5450f1a221f0755f77f9de91f4f33f9e3dba8
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-10 20:35:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,26 @@ button.onclick = function(){
 request.open('GET','http://aspiringvignesh.imad.hasura-app.io/counter');
 request.send('null');
 };
+var submit = document.getElementById("submitb");
+submit.onclick = function(){
+    var request =new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(vari=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                    var ul =document.getElementById("list");
+                    ul.innerHTML=names;
+                    
+                
+            }
+        }
+    };
+
+request.open('GET','http://aspiringvignesh.imad.hasura-app.io/submitname?name=' +name,true);
+request.send('null');
+};
