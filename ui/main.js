e0d7479e57332710ca6fd1419e9fd677de38a70a
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-10 10:07:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,4 @@
 console.log('Loaded!');
-//changing the text
-var element = document.getElementById('maint');
-element.innerHTML='This Website is Blocked';
 //moving the image
 var move=0;
 function moveright(){
