79988d806813d4fce4c11164540e633b51ee6ca4
vigneshdgv <30751063+vigneshdgv@users.noreply.github.com>
2017-08-10 22:27:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function(){
                 var names=request.responseText;
                 names=JSON.parse(names);
                 var list='';
-                for(vari=0;i<names.length;i++){
+                for(var i=0;i<names.length;i++){
                     list+='<li>'+names[i]+'</li>';
                 }
                     var ul =document.getElementById("list");
